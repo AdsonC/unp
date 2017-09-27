c2344a52986aac339140716f1ae3f9e8c2881032
Adson <adsoncarvalho96@gmail.com>
2017-09-27 13:55:59 -0300

    Aula 4 Formulario
    Aprendizado aula 4

---
+++ php/Aula 4 - Entrada de informação (Formulario)/formulario/index.php
@@ -0,0 +1,45 @@
+<html>
+    
+    
+    <head>
+        <title> Meu Formulario</title>
+
+    <body>
+        <h1>Formulário</h1>
+        
+        <form action="resultado.php">
+            
+            <label>Nome:</label>
+            <input type="text" name="nome" placeholder="Digite seu nome"> <br/> <br/>
+    
+                
+            <label>Digite o primeiro valor</label>
+            <input type="number" name="valor1">
+        
+            <label>Digite o segundo valor</label>
+            <input type="number" name="valor2">
+            
+<!--            para pular uma linha-->
+            
+            <br/>
+           
+<!--            criar botao-->
+            
+            <button type="submit">Enviar</button>
+        
+        
+        </form>
+    
+    
+    
+    </body>
+        
+    
+    </head>
+    
+
+</html>
+
+
+
+
---
+++ php/Aula 4 - Entrada de informação (Formulario)/formulario/resultado.php
@@ -0,0 +1,8 @@
+<?php
+$valor1Recebido = $_GET['valor1'];
+$valor2Recebido = $_GET['valor2'];
+    echo "O valor da multiplicação é " . $valor1Recebido * $valor2Recebido;
+
+
+
+?>
